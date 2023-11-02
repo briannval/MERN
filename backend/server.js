965e00bc588a96cd2ff7305c93e5fb4b748e553b
BRIAN V <brianvalentinoadhitya@gmail.com>
2023-11-01 20:28:16 -0700

    frontend connects

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,14 @@ require('dotenv').config()
 
 const express = require('express');
 const mongoose = require('mongoose')
+const cors = require('cors');
 const workoutRoutes = require("./routes/workouts")
 
 // express app
 const app = express();
 
 // middleware
+app.use(cors());
 app.use(express.json())
 
 app.use((req, res, next) => {
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,31 @@
+import { useEffect, useState } from "react"
+import axios from 'axios';
+
+// components
+import WorkoutDetails from "../components/WorkoutDetails"
+
+
 const Home = () => {
-    return (
-        <div className="home">
-            <h2>Home</h2>
-        </div>
-    )
+  const [workouts, setWorkouts] = useState(null)
+
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const response = await axios.get('http://localhost:4000/api/workouts')
+      console.log(response.data)
+    }
+
+    fetchWorkouts()
+  }, [])
+
+  return (
+    <div className="home">
+      <div className="workouts">
+        {workouts && workouts.map(workout => (
+          <WorkoutDetails workout={workout} key={workout._id} />
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default Home
